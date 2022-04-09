6b86c43b3893870a9cc40395e6248f16c7dd62ee
Billy Ponto <bpgponto@gmail.com>
2022-04-09 13:40:37 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,8 +31,7 @@
 <script>
 // @ is an alias to /src
 import PostList from "../components/PostList.vue";
-import { ref } from "vue";
-
+import getPosts from "../composable/getPosts";
 export default {
 	name: "HomeView",
 	components: {
@@ -54,23 +53,8 @@ export default {
 		// return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
 
 		// PART 2
-		const posts = ref([]);
-		const error = ref(null);
-
-		const load = async () => {
-			try {
-				let data = await fetch("http://localhost:3000/posts");
-				// console.log(data);
-				if (!data.ok) {
-					throw Error("No data available");
-				}
-
-				posts.value = await data.json();
-			} catch (err) {
-				error.value = err.message;
-				console.log(error.value);
-			}
-		};
+		// destructure the value from getPosts
+		const { posts, error, load } = getPosts();
 
 		load();
 
